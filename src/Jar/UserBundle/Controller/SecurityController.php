d5ecc6bc826a547ec6d0d22a8ee37d893619883a
Jacek Rutkowski <rutkowski.j.a@gmail.com>
2013-11-02 23:28:55 +0100

    Modifie userBundle
    Add new controller and mew methods in UserController

---
+++ src/Jar/UserBundle/Controller/SecurityController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Jar\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SecurityController extends Controller
+{
+    public function loginAction()
+    {
+    }
+
+    public function logoutAction()
+    {
+    }
+
+}
--- src/Jar/UserBundle/Controller/UserController.php
+++ src/Jar/UserBundle/Controller/UserController.php
@@ -6,8 +6,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class UserController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('JarUserBundle:User:index.html.twig', array('name' => $name));
+        $this->forward("list");
+        //return $this->render('JarUserBundle:User:index.html.twig', array('name' => "asdf"));
+    }
+
+    public function listAction()
+    {
+        return $this->render('JarUserBundle:User:index.html.twig', array('name' => "asdf"));
+    }
+
+    public function crateAction()
+    {
+    }
+
+    public function updateAction()
+    {
+    }
+
+    public function removeAction()
+    {
     }
 }
